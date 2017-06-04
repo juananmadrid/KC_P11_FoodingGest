ce4ed1b4090d0485b1d3443c2e5b0565f3b62932
Juanan_iMac <juananmadrid@gmail.com>
2017-06-04 19:47:51 +0200

    Creamos proyecto, paquetes y primera actividad TablesActivity

---
+++ activity/TablesActivity.java
@@ -0,0 +1,15 @@
+package es.kronox.foodinggest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import es.kronox.foodinggest.R;
+
+public class TablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tables);
+    }
+}
